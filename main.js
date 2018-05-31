ab5f4b909ab1d67aa5324d6b01b13a21f4147f21
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2018-05-31 13:15:07 +0200

    show bar

--- main.js
+++ main.js
@@ -1,48 +1,36 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded",loadScript);
+document.addEventListener("DOMContentLoaded", loadScript);
 
-async function loadScript(){
+async function loadScript() {
 
-        let jsonObject = await fetch("https://kea-alt-del.dk/kata-distortion/");
-        let myJson = await jsonObject.json();
-
-
-        console.log(myJson);
-
-       
-
-        function show(){
-
-            //console.log(queue)
-        
-            document.querySelector('.bar').textContent=`${myJson.inQueue}`;
-        
-           // let queueBar = document.querySelector('.bar').content;
-        
-        };
+    let jsonObject = await fetch("https://kea-alt-del.dk/kata-distortion/");
+    let myJson = await jsonObject.json();
 
 
-        show();
-        
+    console.log(myJson);
+
+
+
+    function show() {
+
+        //console.log(queue)
+
+        document.querySelector('.barnumber').textContent = `There are ${myJson.inQueue} people in line.`;
+        document.querySelector('.bar').style.width = `${myJson.inQueue}0px`;
+
+
+        // let queueBar = document.querySelector('.bar').content;
+
+    };
+
+
+    show();
+
 
 };
 
 
-
-// function show(queue){
-
-//     //console.log(queue)
-
-//     document.querySelector('.bar').textContent=`${myJson.inQueue}`;
-
-//    // let queueBar = document.querySelector('.bar').content;
-
-// };
-
-
-setInterval(function(){ 
-    loadScript();    
+setInterval(function () {
+    loadScript();
 }, 10000);
-
-
