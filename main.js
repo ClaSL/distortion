397b3a8c5741a7d8bec964afe5204e06a15fe017
Clarissa Schwarz-Linnet <clarissa.schwarz@gmx.net>
2018-05-31 14:17:30 +0200

    DONE :-D

--- main.js
+++ main.js
@@ -16,11 +16,19 @@ async function loadScript() {
 
         //console.log(queue)
 
-        document.querySelector('.barnumber').textContent = `There are ${myJson.inQueue} people in line.`;
+        document.querySelector('.barnumber').textContent = `Number of people in line: ${myJson.inQueue}`;
         document.querySelector('.bar').style.width = `${myJson.inQueue}0px`;
 
+            if (myJson.inQueue>20){
+                document.querySelector('.text').textContent = `You'll turn to dust in this queue.. Go for a cocktail instead.`
+            } else if (myJson.inQueue<7){
+                document.querySelector('.text').textContent = `Hurry up! It's now or never!`
+            } else {
+                document.querySelector('.text').textContent = `Think about some good concersation topic to entertain the queue while waiting.`
 
-        // let queueBar = document.querySelector('.bar').content;
+            }
+
+       
 
     };
 
